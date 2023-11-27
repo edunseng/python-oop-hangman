53332a02cd9bc7759a1f93d1bff965972df61261
Hector <hector.edunseng@outlook.com>
2023-11-27 21:04:32 +0000

    adds check functions. ask_for_input() and check_guess().

--- hangman/milestones.py
+++ hangman/milestones.py
@@ -3,16 +3,14 @@ import re
 
 #Create a list containing the names of your 5 favorite fruits.
 word_list = ["apple", "banana", "cherry", "mango", "strawberry"]
-#Print out the newly created list to the standard output (screen).
-hit = False
-while (hit == False):
-    word = random.choice(word_list)
-    guess = "l"
-    #guess = input("enter a single letter:")
-    # Create a statement that checks if the input is a single alphabetical letter.
-    guess = re.search('^[a-z,A-Z]$',guess)
+
+def check_guess(guess, word):  
+    # Convert the guess into lower case.
+    guess =  guess.lower()
+    guess = re.search('^[a-z]$',guess)
+    # Check if the guess is in the word.
     hit = word.__contains__(guess.string)
-    match guess:
+    match guess: 
         case None:
             print ('Invalid letter. Please, enter a single alphabetical character.')
         case _:
@@ -20,3 +18,14 @@ while (hit == False):
                 print(f"Good guess! '{guess.string}' is in the word: {word}.")
             else: 
                 print(f"Sorry, '{guess.string}' is not in the word: {word}. Try again.")
+    return hit
+
+def ask_for_input(wordlist=word_list):
+    # Iteratively check if the input is a valid guess.
+    hit = False
+    while (hit == False):
+        word = random.choice(wordlist)
+        guess = input("Enter a single letter:")
+        hit = check_guess(guess, word)
+
+ask_for_input()
